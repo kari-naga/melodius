7ac102c39549c3cb98e8f011a428e838439098a3
Kari Naga <4119937+kari-naga@users.noreply.github.com>
2023-11-18 18:39:21 -0500

    feat: add basic starter UI

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,22 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import node from '@astrojs/node'
 
-import node from "@astrojs/node";
+import tailwind from '@astrojs/tailwind'
+import Icons from 'unplugin-icons/vite'
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  output: 'server',
   adapter: node({
-    mode: "standalone"
-  })
-});
+    mode: 'standalone',
+  }),
+  integrations: [tailwind()],
+  vite: {
+    plugins: [
+      Icons({
+        compiler: 'astro',
+        autoInstall: true,
+      }),
+    ],
+  },
+})
