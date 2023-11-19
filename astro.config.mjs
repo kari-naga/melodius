b7a35ddc696049038863a47abaa4b181e74f911a
Kari Naga <4119937+kari-naga@users.noreply.github.com>
2023-11-19 04:58:03 -0500

    feat: add working version of homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,24 @@
-import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
-import tailwind from '@astrojs/tailwind';
-import Icons from 'unplugin-icons/vite';
-import vue from "@astrojs/vue";
+import { defineConfig } from 'astro/config'
+import node from '@astrojs/node'
+import tailwind from '@astrojs/tailwind'
+import Icons from 'unplugin-icons/vite'
+import vue from '@astrojs/vue'
 
-import react from "@astrojs/react";
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: node({
-    mode: 'standalone'
+    mode: 'standalone',
   }),
   integrations: [tailwind(), vue(), react()],
   vite: {
-    plugins: [Icons({
-      compiler: 'astro',
-      autoInstall: true
-    })]
-  }
-});
+    plugins: [
+      Icons({
+        compiler: 'raw',
+        autoInstall: true,
+      }),
+    ],
+  },
+})
---
+++ src/components/astro/Menu.vue
@@ -0,0 +1,26 @@
+<script setup>
+    import IconList from '~icons/ph/list-fill'
+    import IconX from '~icons/ph/x-bold'
+    import { ref } from 'vue'
+    const toggle = ref(false)
+</script>
+
+
+<template>
+    <div class="relative">
+        <button @click="toggle = !toggle" ><span v-if="!toggle" v-html="IconList" class="white hover:text-cyan-400 transition-colors" /><span v-if="toggle" v-html="IconX" class="white hover:text-cyan-400 transition-colors" /></button>
+        <div class="absolute">
+            <h1 v-if="toggle" class="hover: drop-shadow-2xl ">
+                <h1 v-show="toggle">
+                    <!-- <span v-html="IconX" class="white" /> -->
+                    <div class="flex flex-col gap-4 font-light">
+                        <a href="" class="mt-2 hover:text-cyan-400 transition-colors">Home</a>
+                        <a href="" class="hover:text-cyan-400 transition-colors">Create</a>
+                        <a href="" class="hover:text-cyan-400 transition-colors">About</a>
+                    </div>
+                </h1>
+            </h1>
+            <h1 v-else class="hover: drop-shadow-2xl"></h1>
+        </div>          
+    </div>
+</template>
